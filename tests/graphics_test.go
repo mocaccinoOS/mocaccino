4d5dd8c0bb79f592e3b1e64e86a80a8d6e7fd0d5
Ettore Di Giacinto <mudler@sabayon.org>
2022-01-22 17:33:51 +0100

    Run minimal tests on each run, add graphics test

---
+++ tests/graphics_test.go
@@ -0,0 +1,20 @@
+package mos_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("MocaccinoOS X", func() {
+	BeforeEach(func() {
+		eventuallyConnects()
+	})
+
+	Context("Graphics", func() {
+		It("installs gnome", func() {
+			out, err := sshCommand("luet install -y layers/gnome")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(out).Should(ContainSubstring("installed"))
+		})
+	})
+})
